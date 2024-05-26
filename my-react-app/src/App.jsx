7990ae3e030d4dd4f6298f93275e801f3d4ede00
EVERT-ANDERSON\Evert <evert.anderson.hl@gmail.com>
2024-05-25 20:19:11 -0500

    Update for conditional rendering

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./Footer";
 import Food from "./Food";
 import Button from "../Button/Button";
 import Student from "./Student.jsx"
+import UserGreeting from "./UserGreeting.jsx";
 
 function App() {
   return(
@@ -14,11 +15,15 @@ function App() {
       <Food />
       <Footer /> */}
       {/* <Button /> */}
-      <Student name="SpongeBob" age={30} isStudent={true} />
+      {/* <Student name="SpongeBob" age={30} isStudent={true} />
       <Student name="Patrik" age={42} isStudent={false} />
       <Student name="Squidward" age={50} isStudent={false} />
       <Student name="Sandy" age={24} isStudent={true} />
-      <Student name="Larry" />
+      <Student name="Larry" /> */}
+      <UserGreeting isLoggedIn={true} username="EvertCode"/>
+      <UserGreeting isLoggedIn={false} username="hacker" />
+      <UserGreeting isLoggedIn={false} />
+
     </>
   );
 }
